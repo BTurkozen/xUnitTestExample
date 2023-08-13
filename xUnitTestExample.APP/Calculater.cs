9d335196439a3c31a5809bfb8fb369232ac5104e
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-13 21:19:00 +0300

    - Hesaplama işlemi için bir nesne oluşturuldu. - Oluşturulan nesne için test aşaması yazılarak test edildi. - Test aşamasında Equal ve NotEqual Assertleri kullanıldı.

---
+++ xUnitTestExample.APP/Calculater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTestExample.APP
+{
+    public class Calculater
+    {
+        public int add(int numberFirst, int numberSecond)
+        {
+            return numberFirst + numberSecond;
+        }
+    }
+}
---
+++ xUnitTestExample.Test/CalculaterTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xUnitTestExample.APP;
+
+namespace xUnitTestExample.Test
+{
+    public class CalculaterTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            // ----------------------------- Arrange -----------------------------
+
+            // Değişkenleri initialize ettiğimiz yerdir.
+            int numberFirst = 5, numberSecond = 10;
+
+            // Yada nesne örneği oluşturacağımnız yerdir.
+            var calculater = new Calculater();
+
+            // ----------------------------- Act -----------------------------
+
+            // initialize ettiğimiz nesnelerimize parametreler verip test edeceğimiz methodları çalıştıracağımız yerdir.
+            var total = calculater.add(numberFirst, numberSecond);
+
+            // ----------------------------- Assert -----------------------------
+
+            // Act evresinden çıkan sonucun beklenen sonuç mu değil mi evresidir. Testin doğruluğunu kontrol ettiğimiz yerdir.
+            Assert.Equal<int>(15, total);
+            //Assert.NotEqual<int>(15, total);
+        }
+    }
+}
