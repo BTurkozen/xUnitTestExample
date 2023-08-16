3247028d1a19e42ca355fd6ecee0899c797d129b
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-16 21:20:29 +0300

    - MVC projesi oluşturuldu. - Product Entity'si oluşturularak migration işlemleri eklendi.

---
+++ xUnitTestExample.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace xUnitTestExample.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ xUnitTestExample.Web/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace xUnitTestExample.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        [Precision(18, 2)]
+        //[Column(TypeName = "decimal(5, 2)")]
+        public decimal Price { get; set; }
+    }
+}
