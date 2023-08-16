1da8ed36bb6334fcde2642e579c180c4d713bcd6
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-16 23:03:35 +0300

    - ProductsControllerTest nesnesi oluşturuldu ve constructor işlemleri tamamlandı.

---
+++ xUnitTestExample.WebTest/ProductTest/ProductsControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xUnitTestExample.Web.Controllers;
+using xUnitTestExample.Web.Models;
+using xUnitTestExample.Web.Repository;
+
+namespace xUnitTestExample.WebTest.ProductTest
+{
+    public class ProductsControllerTest
+    {
+        private readonly IMock<IRepository<Product>> _mockRepo;
+
+        private readonly ProductsController _productsController;
+
+        public ProductsControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+            _productsController = new ProductsController(_mockRepo.Object);
+        }
+    }
+}
