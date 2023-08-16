9031d29d80a393ca51297cbc2df248e6e29ac679
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-16 23:26:45 +0300

    - ViewResult ve Viewden dönen product listesi Test edildi.

--- xUnitTestExample.WebTest/ProductTest/ProductsControllerTest.cs
+++ xUnitTestExample.WebTest/ProductTest/ProductsControllerTest.cs
@@ -13,7 +13,7 @@ namespace xUnitTestExample.WebTest.ProductTest
 {
     public class ProductsControllerTest
     {
-        private readonly IMock<IRepository<Product>> _mockRepo;
+        private Mock<IRepository<Product>> _mockRepo;
         private readonly ProductsController _productsController;
         private List<Product> _products;
         public ProductsControllerTest()
@@ -29,11 +29,25 @@ namespace xUnitTestExample.WebTest.ProductTest
         }
 
         [Fact]
-        public async void INdex_ActionExecutes_ReturnView()
+        public async void Index_ActionExecutes_ReturnView()
         {
             var result = await _productsController.Index();
 
             Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async void Index_ActionExecute_ReturnProductList()
+        {
+            _mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(_products);
+
+            var result = await _productsController.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var productList = Assert.IsAssignableFrom<IEnumerable<Product>>(viewResult.Model);
+
+            Assert.Equal<int>(3, productList.Count());
         }
     }
 }
