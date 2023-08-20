5602e398b4d5807910b7a9b4b5d11bf9761cd63c
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-20 21:51:03 +0300

    - Context işlemleri eklendi. - InMemory işlemleri eklendi. - Create ModelValid durumu test edildi.

---
+++ xUnitTestExample.WebTest/ProductTest/ProductsWithCategoryControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xUnitTestExample.Web.Models;
+
+namespace xUnitTestExample.WebTest.ProductTest
+{
+    public class ProductsWithCategoryControllerTest
+    {
+        protected DbContextOptions<DataContext> _dbContextOptions { get; private set; }
+
+        public void SetContextOptions(DbContextOptions<DataContext> dbContextOptions)
+        {
+            _dbContextOptions = dbContextOptions;
+            SeedData();
+        }
+
+        public void SeedData()
+        {
+            using var context = new DataContext(_dbContextOptions);
+
+            // Ayağa kalkınca Db siler
+            context.Database.EnsureDeleted();
+            // Yeniden oluşturur.
+            context.Database.EnsureCreated();
+
+            context.Categories.Add(new Category { Name = "Electronics" });
+            context.Categories.Add(new Category { Name = "Glassware" });
+
+            context.Products.Add(new Product { CategoryId = 1, Name = "Pens", Price = 10, Quantity = 10, Color = "Red" });
+            context.Products.Add(new Product { CategoryId = 2, Name = "Computers", Price = 250, Quantity = 3, Color = "Gray" });
+            context.Products.Add(new Product { CategoryId = 1, Name = "Easer", Price = 5, Quantity = 15, Color = "Green" });
+            context.Products.Add(new Product { CategoryId = 2, Name = "Mouses", Price = 15, Quantity = 100, Color = "Gray" });
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ xUnitTestExample.WebTest/ProductTest/ProductsWithCategoryControllerWithInMemory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xUnitTestExample.Web.Controllers;
+using xUnitTestExample.Web.Models;
+
+namespace xUnitTestExample.WebTest.ProductTest
+{
+    public class ProductsWithCategoryControllerWithInMemory : ProductsWithCategoryControllerTest
+    {
+        public ProductsWithCategoryControllerWithInMemory()
+        {
+            SetContextOptions(new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase("xUnitDb").Options);
+        }
+
+        [Fact]
+        public async void Create_ModelValidProduct_ReturnRTAWithProduct()
+        {
+            var newProduct = new Product() { Name = "Pen 7A", Price = 200, Quantity = 100, Color = "Red" };
+
+            using var context = new DataContext(_dbContextOptions);
+
+            var category = context.Categories.First();
+
+            newProduct.CategoryId = category.Id;
+
+            var controller = new ProductsWithCategoryController(context);
+
+            var result = await controller.Create(newProduct);
+
+            var redirect = Assert.IsType<RedirectToActionResult>(result);
+
+            Assert.Equal("Index", redirect.ActionName);
+
+            // Burada ikinci context oluşturma sebebimiz. Dataları trackladiği için test yanılma ıhtımalıni aradan cıkartmak için.
+            using var contextCheck = new DataContext(_dbContextOptions);
+
+            var product = contextCheck.Products.First(p => p.Name == newProduct.Name);
+
+            Assert.Equal(newProduct.Name, product.Name);
+        }
+    }
+}
